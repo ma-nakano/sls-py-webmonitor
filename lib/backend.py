351bcda72b999c82d8ce81969a2302ad60ede2da
Kaoru Nagashima <git.police6623@gmail.com>
2017-12-06 14:46:19 +0900

    sns機能 追加

--- lib/backend.py
+++ lib/backend.py
@@ -16,20 +16,19 @@ def probe(url):
 
 def check_sites(event, context):
     sites = dynamo.get_sites()
-
     for site in sites:
         status_code = probe(site["url"])
 
         if status_code != site["code"]:
             dynamo.update_site(site["id"], status_code)
             sns(site["id"])
-
+            print (site)
 
 def sns(event):
     client = boto3.client('sns')
 
     response = client.publish(
-        TopicArn=os.environ['SNS_TOPIC_ARN'],
+        TopicArn=os.environ["SNS_TOPIC_ARN"],
         Message='it is test',
         Subject='Test!',
     )
